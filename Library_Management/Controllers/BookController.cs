eeb86bc064a7727a05eb08e2e4e40a85922f63c3
Pooja-72 <poojabathla679@gmail.com>
2021-07-14 14:38:18 +0530

    Book and user API validation

--- Library_Management/Controllers/BookController.cs
+++ Library_Management/Controllers/BookController.cs
@@ -11,96 +11,92 @@ namespace Library_Management.Controllers
     public class BookController : ApiController
     {
         DAL d = new DAL();
+        [Route("api/AllBooks")]
         [HttpGet]
         public HttpResponseMessage getAllBooks()
         {
-            if (d.getAllBooks().Count == 0)
-                return Request.CreateResponse(HttpStatusCode.NotFound, "No Books!");
+            if (ModelState.IsValid)
+            {
+                if (d.getAllBooks().Count == 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Record Doesnot exist.");
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, d.getAllBooks());
+                }
+            }
             else
-                return Request.CreateResponse(HttpStatusCode.OK, d.getAllBooks());
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid data");
+            }
         }
 
         [Route("api/AddBook")]
         [HttpPost]
         public HttpResponseMessage addBook(BOOK book)
         {
-            Library_ManagementEntities entities = new Library_ManagementEntities();
-            BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ISBN == book.ISBN && book1.TITLE == book.TITLE && book1.AUTHOR == book.AUTHOR);
-            if (ifBookExist == null)
+            if (ModelState.IsValid)
             {
+                Library_ManagementEntities entities = new Library_ManagementEntities();
+                BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ISBN == book.ISBN && book1.TITLE == book.TITLE && book1.AUTHOR == book.AUTHOR);
+                if (ifBookExist == null)
+                {
 
-                //try
-                //{
-                    if (ModelState.IsValid)
+                    try
                     {
                         entities.BOOKs.Add(book);
                         entities.SaveChanges();
                         return Request.CreateResponse(HttpStatusCode.OK, "Book was added successfully");
                     }
-                    else
+                    catch (Exception e)
                     {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
                     }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "This Book was already added in the database.");
+                }
 
-                //}
-               // catch (Exception e)
-               // {
-               //     return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
-               // }
             }
             else
             {
-                return Request.CreateResponse(HttpStatusCode.OK, "This Book was already added in the database.");
-
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
             }
-
-
-            //if (ifBookExist != null)
-            //{
-            //    ifBookExist.AVAILABLE_COPIES = book.AVAILABLE_COPIES + ifBookExist.AVAILABLE_COPIES;
-            //    ifBookExist.TOTAL_COPIES = book.TOTAL_COPIES + ifBookExist.TOTAL_COPIES;
-            //    entities.SaveChanges();
-            //    return Request.CreateResponse(HttpStatusCode.OK, "This Book was already added in the database. Total no of copies now available are " + book.AVAILABLE_COPIES );
-            //}
-            //else
-            //{
-            //    entities.BOOKs.Add(book);
-            //    entities.SaveChanges();
-            //    return Request.CreateResponse(HttpStatusCode.OK, "Book was added successfully");
-            //}
         }
 
         [Route("api/UpdateBook")]
         [HttpPut]
         public HttpResponseMessage updateBook(UpdateRack book)
         {
-            Library_ManagementEntities entities = new Library_ManagementEntities();
-            BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID==book.ID);
-            if (ifBookExist == null)
+            if (ModelState.IsValid)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist.");
+                Library_ManagementEntities entities = new Library_ManagementEntities();
+                BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == book.ID);
+                if (ifBookExist == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist.");
+                }
+                else
+                {
+
+                    try
+                    {
+
+                        ifBookExist.RACK_NO = book.RACK_NO;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Book record has been updated");
+                    }
+                    catch (Exception e)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
+                    }
+                }
             }
             else
             {
-
-                try
-                {
-                    if (ModelState.IsValid)
-                    {
-                        ifBookExist.RACK_NO = book.RACK_NO;
-                        entities.SaveChanges();
-                        return Request.CreateResponse(HttpStatusCode.OK, "Rack has been updated");
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
-                    }
-
-                }
-                catch (Exception e)
-                {
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
-                }
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
             }
         }
 
@@ -108,14 +104,13 @@ namespace Library_Management.Controllers
         [HttpDelete]
         public HttpResponseMessage deleteBook(int id)
         {
-            Library_ManagementEntities entities = new Library_ManagementEntities();
-            BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == id);
-            if (ifBookExist != null)
+            if (ModelState.IsValid)
             {
-
-                try
+                Library_ManagementEntities entities = new Library_ManagementEntities();
+                BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == id);
+                if (ifBookExist != null)
                 {
-                    if (ModelState.IsValid)
+                    try
                     {
                         if (ifBookExist.AVAILABLE_COPIES == ifBookExist.TOTAL_COPIES)
                         {
@@ -128,21 +123,19 @@ namespace Library_Management.Controllers
                             return Request.CreateResponse(HttpStatusCode.BadRequest, "This Book cannot be deleted as it has been issuedby a user.");
                         }
                     }
-                    else
+                    catch (Exception e)
                     {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
                     }
-
                 }
-                catch (Exception e)
+                else
                 {
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist");
                 }
-
             }
             else
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
             }
         }
 
@@ -150,33 +143,32 @@ namespace Library_Management.Controllers
         [HttpPost]
         public HttpResponseMessage addCopy(BOOK book)
         {
-            Library_ManagementEntities entities = new Library_ManagementEntities();
-            BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == book.ID);
-            if (ifBookExist == null)
+            if (ModelState.IsValid)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist. Please add the following book in your db.");
-            }
-            else
-            {
-                try
+                Library_ManagementEntities entities = new Library_ManagementEntities();
+                BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == book.ID);
+                if (ifBookExist == null)
                 {
-                    if (ModelState.IsValid)
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist. Please add the following book in your db.");
+                }
+                else
+                {
+                    try
                     {
                         ifBookExist.TOTAL_COPIES = ifBookExist.TOTAL_COPIES + 1;
                         ifBookExist.AVAILABLE_COPIES = ifBookExist.AVAILABLE_COPIES + 1;
                         entities.SaveChanges();
                         return Request.CreateResponse(HttpStatusCode.OK, "Book record has been updated");
                     }
-                    else
+                    catch (Exception e)
                     {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
                     }
-
                 }
-                catch (Exception e)
-                {
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
-                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
             }
         }
 
@@ -184,23 +176,23 @@ namespace Library_Management.Controllers
         [HttpPost]
         public HttpResponseMessage deleteCopy(BOOK book)
         {
-            Library_ManagementEntities entities = new Library_ManagementEntities();
-            BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == book.ID);
-            if (ifBookExist == null)
+            if (ModelState.IsValid)
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist. Please add the following book in your db.");
-            }
-            else
-            {
-                try
+                Library_ManagementEntities entities = new Library_ManagementEntities();
+                BOOK ifBookExist = entities.BOOKs.FirstOrDefault(book1 => book1.ID == book.ID);
+                if (ifBookExist == null)
                 {
-                    if (ModelState.IsValid)
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "This book does not exist. Please add the following book in your db.");
+                }
+                else
+                {
+                    try
                     {
                         if (ifBookExist.TOTAL_COPIES >= ifBookExist.AVAILABLE_COPIES && ifBookExist.AVAILABLE_COPIES > 0)
                         {
                             if (ifBookExist.TOTAL_COPIES == 1 && ifBookExist.AVAILABLE_COPIES == 1)
                             {
-                                entities.BOOKs.Remove(book);
+                                entities.BOOKs.Remove(ifBookExist);
                                 entities.SaveChanges();
                                 return Request.CreateResponse(HttpStatusCode.OK, "Book record has been deleted");
                             }
@@ -217,16 +209,15 @@ namespace Library_Management.Controllers
                             return Request.CreateResponse(HttpStatusCode.BadRequest, "This Book cannot be deleted as it has been issuedby a user.");
                         }
                     }
-                    else
+                    catch (Exception e)
                     {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
+                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
                     }
-
                 }
-                catch (Exception e)
-                {
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid data");
-                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid details.");
             }
         }
     }
--- Library_Management/Models/Model1.Designer.cs
+++ Library_Management/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\SarthakGoyal\source\repos\Library_Management\Library_Management\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\PoojaBathla\Documents\C Sharp coding practice\Library_Backend\Library_Management\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
